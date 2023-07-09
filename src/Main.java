322c5094a99f09c2ec6d2a16a36adbdad66183ea
Alexmzrnk <alex.mzrnk@gmail.com>
2023-07-09 11:01:54 -0400

    the feature of planet1 added

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,12 @@ public class Main {
         AdamX.temp="123456 celcius";
         AdamX.size="32465km";
 
+
+        Planet1 xeon= new Planet1();
+        xeon.name="reon";
+        xeon.color="pink";
+        xeon.temp="-45celcius";
+        xeon.size="1cm";
+
     }
 }
